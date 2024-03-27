58ef958891360ed571c34c824124dd9c9d4eca8b
thai vo <thaivo88@gmail.com>
2024-03-27 13:37:40 -0500

    Create sine.c

---
+++ sine.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// math header library to use the sin function
+#include <math.h>
+
+int main()
+{
+// declaration x,y with a start value of 0
+  double x = 0;
+  double y = 0;
+
+// prompting a user to input a value
+  printf("In put a positive float value from 0 to 1: ");
+  scanf("%lf", &x);
+
+// calculating sine function and outputing value of sine of x
+  y = sin(x);
+  printf("\nThe Sine of %lf, is %lf", x, y);
+
+  return 0;
+}
